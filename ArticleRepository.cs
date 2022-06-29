8148e15c2a152ed77c6be16d771ab7e46d6bc626
Alfred-Paul <56517532+Alfred-Paul@users.noreply.github.com>
2022-06-29 14:39:39 +1000

    Added Middleware sample to repo

---
+++ ArticleRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class ArticleRepository : IArticleRepository
+    {
+        private List<Article> Articles { get; set; }
+        
+        public ArticleRepository()
+        {
+            Articles = new List<Article>();
+        }
+
+        public Article GetArticle()
+        {
+            return Articles.FirstOrDefault();
+        }
+
+        IEnumerable<Article> IArticleRepository.GetArticles()
+        {
+            return Articles;
+        }
+
+        public void AddArticle(Article article)
+        {
+            Articles.Add(article);
+        }
+
+        public void SetArticleList(List<Article> articles)
+        {
+            Articles = articles;
+        }
+    }
+}
---
+++ IArticleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public interface IArticleRepository
+    {
+        public Article GetArticle();
+
+        public IEnumerable<Article> GetArticles();
+
+        public void AddArticle(Article article);
+
+        public void SetArticleList(List<Article> articles);
+    }
+}
